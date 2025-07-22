3a3a90bc0f44f353ac3d807696b1b3186ced0004
kilix <shauryabhalla.fr@gmail.com>
2025-07-23 00:01:58 +0530

    werent being counted, I swear I did alot of changes over the past 3 days

--- ephemeral-chat/public/chat.js
+++ ephemeral-chat/public/chat.js
@@ -60,4 +60,4 @@ closeBtn.addEventListener("click", () => {
   searchOverlay.style.pointerEvents = "none";
 });
 
-// I just realised my commits weren't 
+// I just realised my commits weren't being counted
